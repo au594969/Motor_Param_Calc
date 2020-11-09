b551eaaf5cd169324669d7b6db728fa76a137fc1
Christian Moos Lund <au594969@uni.au.dk>
2020-11-09 22:46:55 +0100

    OhmCalculator with If statements done

--- OhmsLaw_Calculator.py
+++ OhmsLaw_Calculator.py
@@ -1,30 +1,22 @@
 #Sandbox test code for implementation for the real calculator
 
 import numpy as np
-import cmath
-
-
 
 R = input("Enter the Resistanc value (if uknown enter 0): ")
-V = input("Enter the Voltage value(if uknown enter 0): ")
-I = input("Enter the Current value(if uknown enter 0): ")
+V = input("Enter the Voltage value (if uknown enter 0): ")
+I = input("Enter the Current value (if uknown enter 0): ")
 #P = input("Enter the Power value(if uknown enter 0): ")
 
+if float(R) == 0:
+	Calc_R = float(V) / float(I)
+	print("The Resistance value is: %f " %Calc_R)
 
-if R == 0
-	 
-	 Calc_R = V/I
+if float(V) == 0:
+	Calc_V = float(R) * float(I)
+	print("The Voltage value is: %f " %Calc_V)
 
-	 Print("The Resistanc value is: {} ").format(Calc_R)
-elif V == 0
-	
-	Calc_V = R*I
+if float(I) == 0:
+	Calc_I = float(V) / float(R)
+	print("The Current value is: %f " %Calc_I)
 
-	Print("The Voltage value is: {} ").format(Calc_V)
-
-else I == 0
-
-	Calc_I = V/I
-
-	Print("The Voltage value is: {} ").format(Calc_V)
-
+print("Calculation is done")
